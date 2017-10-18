3e93975c0609c0f3d3b8fb13df33fc63cbf2fc77
RoxVer <rshveda15@gmail.com>
2017-10-18 21:03:53 +0300

    Product Order Test

---
+++ src/test/java/selenium/grid/app/BaseTest.java
@@ -0,0 +1,110 @@
+package selenium.grid.app;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    String device = "Nexus 5X";
+
+    private WebDriver getDriver(String browser) {
+        DesiredCapabilities capabilities;
+        if (browser.equals("firefox")) {
+            System.setProperty(
+                    "webdriver.gecko.driver",
+                    new File(BaseTest.class.getResource("/geckodriver.exe").getFile()).getPath());
+            return new FirefoxDriver();
+        } else if (browser.equals("ie")) {
+            System.setProperty(
+                    "webdriver.ie.driver",
+                    new File(BaseTest.class.getResource("/IEDriverServer.exe").getFile()).getPath());
+            capabilities = DesiredCapabilities.internetExplorer();
+            capabilities.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false);            // disable native events to speed up typing
+            capabilities.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);   // force clean session
+            return new InternetExplorerDriver(new InternetExplorerOptions(capabilities));
+        } else if (browser.equals("phantomjs")) {
+            System.setProperty(
+                    "phantomjs.binary.path",
+                    new File(BaseTest.class.getResource("/phantomjs.exe").getFile()).getPath());
+            return new PhantomJSDriver();
+        } else if (browser.equals("android")) {
+            System.setProperty(
+                    "webdriver.chrome.driver",
+                    new File(BaseTest.class.getResource("/chromedriver.exe").getFile()).getPath());
+
+            Map<String, String> mobileEmulation = new HashMap<String, String>();
+            mobileEmulation.put("deviceName", device);
+
+            Map<String, Object> chromeOptions = new HashMap<String, Object>();
+            chromeOptions.put("mobileEmulation", mobileEmulation);
+            capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+            return new ChromeDriver(capabilities);
+        } else {
+            System.setProperty(
+                    "webdriver.chrome.driver",
+                    new File(BaseTest.class.getResource("/chromedriver.exe").getFile()).getPath());
+            return new ChromeDriver();
+        }
+    }
+
+    private RemoteWebDriver getRemoteDriver(String hubUrl, String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities;
+        if (browser.equals("firefox")) {
+            capabilities = DesiredCapabilities.firefox();
+        } else if (browser.equals("ie")) {
+            capabilities = DesiredCapabilities.internetExplorer();
+        } else if (browser.equals("phantomjs")) {
+            capabilities = DesiredCapabilities.phantomjs();
+        } else if (browser.equals("android")) {
+            Map<String, String> mobileEmulation = new HashMap<String, String>();
+            mobileEmulation.put("deviceName", device);
+
+            Map<String, Object> chromeOptions = new HashMap<String, Object>();
+            chromeOptions.put("mobileEmulation", mobileEmulation);
+            capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+            //driver = new ChromeDriver(capabilities);
+        } else {
+            capabilities = DesiredCapabilities.chrome();
+        }
+        return new RemoteWebDriver(new URL(hubUrl), capabilities);
+    }
+
+    @BeforeClass
+    @Parameters({"selenium.hub", "selenium.browser"})
+    public void setUp(@Optional("") String hubURL, @Optional("android") String browser) throws MalformedURLException {
+        driver = hubURL.isEmpty() ? getDriver(browser) : getRemoteDriver(hubURL, browser);
+        //hubURL = http://localhost:4444/wd/hub
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    /*@AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }*/
+}
---
+++ src/test/java/selenium/grid/app/tests/OrderTest.java
@@ -0,0 +1,258 @@
+package selenium.grid.app.tests;
+
+import org.apache.commons.lang.WordUtils;
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import selenium.grid.app.BaseTest;
+import selenium.grid.app.utils.CustomReporter;
+import selenium.grid.app.utils.Properties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class OrderTest extends BaseTest {
+    int randomNumber;
+    int productsQ;
+    int previousQuantity;
+    int finalQuantity;
+    String nameP;
+    SoftAssert asser = new SoftAssert();
+    Random rand = new Random();
+    String randName;
+    String domain = "gmail.com";
+    List<String> products = new ArrayList<String>();
+    List<WebElement> elements;
+    int id;
+
+    @Test
+    public void checkVersion(String browser) {
+        CustomReporter.logAction("Check version test starts");
+        driver.navigate().to(Properties.getBaseUrl());
+        //driver.navigate().refresh();
+        if (browser.equals("android") && driver.findElement(By.id("menu-icon")).isDisplayed()) {
+            CustomReporter.log("PASSED: Mobile version is opened correctly");
+        } else if (browser.equals("chrome") && !(driver.findElement(By.id("menu-icon")).isDisplayed())) {
+            CustomReporter.log("PASSED: Desktop version opened correctly");
+        } else if (browser.equals("android") && !(driver.findElement(By.id("menu-icon")).isDisplayed())) {
+            CustomReporter.log("FAILED: Mobile version opened incorrectly");
+        } else if (browser.equals("chrome") && driver.findElement(By.id("menu-icon")).isDisplayed()) {
+            CustomReporter.log("FAILED: Desktop version opened incorrectly");
+        }
+    }
+
+    @Test
+    public void checkOrder() {
+        CustomReporter.logAction("checkOrder test starts");
+        CustomReporter.logAction("Open URL");
+        driver.navigate().to(Properties.getBaseUrl());
+        bigScroll();
+        CustomReporter.logAction("Click All products button");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#content > section > a")));
+        driver.findElement(By.cssSelector("#content > section > a")).click();
+        //Check products quantity on a page
+        WebElement element = driver.findElement(By.cssSelector("div.col-sm-12.hidden-md-up.text-xs-center.showing"));
+        String prod = element.getText();
+        prod = prod.substring(prod.lastIndexOf(" ") + 1);
+        prod = prod.replaceAll("\\D+","");
+        productsQ = 0;
+        try {
+            productsQ = Integer.parseInt(prod);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Number Format Exception <br />");
+        }
+        for (int i = 0; i < productsQ; i++) {
+            elements = driver.findElements(By.xpath("//h1/a"));
+            products.add(elements.get(i).getText());
+        }
+        CustomReporter.logAction("Click random product");
+        id = randomNumber();
+        scrollUntilVisible(elements.get(id));
+        elements.get(id).click();
+        smallScroll();
+        CustomReporter.logAction("Click info about product");
+        driver.findElement(By.cssSelector("li:nth-child(2) > a.nav-link")).click();
+        //Info about product on product page
+        //Quantity
+        String previousQString = driver.findElement(By.cssSelector("div.product-quantities > span")).getText();
+        previousQString = previousQString.replaceAll("\\D+","");
+        previousQuantity = 0;
+        try {
+            previousQuantity = Integer.parseInt(previousQString);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Previous Quantity Format Exception");
+        }
+        //Price
+        String priceEl = driver.findElement(By.cssSelector("div.product-price.h5 > div > span")).getText();
+        priceEl = priceEl.replaceAll("\\D+","");
+        int priceP = 0;
+        try {
+            priceP = Integer.parseInt(priceEl);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Price Format Exception");
+        }
+        //Name
+        nameP = driver.findElement(By.cssSelector("h1.h1")).getText();
+
+        CustomReporter.logAction("Add product to basket");
+        driver.findElement(By.cssSelector("div.add > button")).click();
+        By modalButton = By.cssSelector("div.modal-body > div > div:nth-child(2) > div > a");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(modalButton));
+
+        CustomReporter.logAction("Click Make Order button");
+        driver.findElement(modalButton).click();
+
+        CustomReporter.logAction("Compare added product with the basket product");
+        //Basket product quantity
+        String qty = driver.findElement(By.xpath("//*[@id=\"cart-subtotal-products\"]/span[1]")).getText();
+        qty = qty.replaceAll("\\D+","");
+        int quant = 0;
+        try {
+            quant = Integer.parseInt(qty);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Number Format Exception <br />");
+        }
+        asser.assertEquals(quant, 1, "Incorrect quantity: ");
+        //Basket product name
+        String basketName = driver.findElement(By.cssSelector("span > img")).getText();
+        asser.assertEquals(basketName, nameP, "Incorrect name: ");
+        //Basket product price
+        String actualPrice = driver.findElement(By.cssSelector("strong")).getText();
+        actualPrice = actualPrice.replaceAll("\\D+","");
+        int basketPrice = 0;
+        try {
+            basketPrice = Integer.parseInt(actualPrice);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Price Format Exception");
+        }
+        asser.assertEquals(basketPrice, priceP, "Incorrect price: ");
+
+        CustomReporter.logAction("Fill personal data");
+        driver.findElement(By.cssSelector("div.text-xs-center > a")).click();
+        driver.findElement(By.name("firstname")).sendKeys(getName(randomNumber()));
+        driver.findElement(By.name("lastname")).sendKeys(getName(randomNumber()));
+        driver.findElement(By.name("email")).sendKeys(getEmail(randomNumber(), domain));
+        smallScroll();
+        driver.findElement(By.name("continue")).click();
+        driver.findElement(By.name("address1")).sendKeys(getName(randomNumber()));
+        driver.findElement(By.name("postcode")).sendKeys(index());
+        driver.findElement(By.name("city")).sendKeys(getName(randomNumber()));
+        smallScroll();
+        driver.findElement(By.name("confirm-addresses")).click();
+        driver.findElement(By.name("confirmDeliveryOption")).click();
+        driver.findElement(By.id("payment-option-2")).click();
+        driver.findElement(By.name("conditions_to_approve[terms-and-conditions]")).click();
+        driver.findElement(By.cssSelector("#payment-confirmation > div.ps-shown-by-js > button")).click();
+        String expectedMessage = "ВАШ ЗАКАЗ ПОДТВЕРЖДЁН";
+        String message = driver.findElement(By.cssSelector("h3.h1.card-title")).getText();
+        asser.assertEquals(message, expectedMessage, "Order confirmation message not found: ");
+
+        String lastQ = driver.findElement(By.cssSelector("div > div.col-xs-2")).getText();
+        asser.assertEquals(lastQ, 1, "Incorrect last quantity: ");
+
+        String lastP = driver.findElement(By.cssSelector("div.col-xs-5.text-xs-right.bold")).getText();
+        lastP = lastP.replaceAll("\\D+","");
+        int lastPrice = 0;
+        try {
+            lastPrice = Integer.parseInt(lastP);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Price Format Exception");
+        }
+        asser.assertEquals(lastPrice, priceP, "Incorrect last price: ");
+
+        String lastName = driver.findElement(By.cssSelector("div.col-sm-4.col-xs-9.details > span")).getText();
+        if(lastName.contains(nameP)) {
+            CustomReporter.log("Last product name is correct");
+        } else {
+            CustomReporter.log("Last product name is NOT correct");
+        }
+        scrollPageDown();
+        CustomReporter.logAction("Click Save button");
+        driver.findElement(By.cssSelector("#content-hook-order-confirmation-footer > section > a")).click();
+        nameP = WordUtils.capitalizeFully(nameP);
+        scrollUntilVisible(driver.findElement(By.xpath("//h1/a[.='" + nameP + "']")));
+        driver.findElement(By.xpath("//h1/a[.='" + nameP + "']")).click();
+        scrollUntilVisible(driver.findElement(By.cssSelector("div.tabs > ul > li:nth-child(2) > a")));
+        driver.findElement(By.cssSelector("div.tabs > ul > li:nth-child(2) > a")).click();
+
+        String finalQString = driver.findElement(By.cssSelector("div.product-quantities > span")).getText();
+        finalQString = finalQString.replaceAll("\\D+","");
+        finalQuantity = 0;
+        try {
+            finalQuantity = Integer.parseInt(finalQString);
+        } catch (NumberFormatException e) {
+            CustomReporter.log("Final Quantity Format Exception");
+        }
+        asser.assertEquals(finalQuantity, previousQuantity, "Incorrect final product quantity: ");
+    }
+
+    public int randomNumber() {
+        randomNumber = new RandomDataGenerator().nextInt(1, productsQ - 1);
+        return randomNumber;
+    }
+
+    public String index() {
+        int ind = 10000 + rand.nextInt(90000);
+        String index = Integer.toString(ind);
+        return index;
+    }
+
+    public String getEmail(int count, String domain) {
+        String chars = "abcdefghijklmnopqrstuvwxyz" + count;
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int index = (int) (rand.nextDouble() * chars.length());
+            builder.append(chars.charAt(index));
+        }
+        randName = builder.toString();
+        randName = randName + "@" + domain;
+        return randName;
+    }
+
+    public String getName(int count) {
+        String chars = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        StringBuilder builder = new StringBuilder();
+        while (count-- != 0) {
+            int index = (int) (rand.nextDouble() * chars.length());
+            builder.append(chars.charAt(index));
+        }
+        String name = builder.toString();
+        return name;
+    }
+
+    public void bigScroll(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement icon = driver.findElement(By.cssSelector("#menu-icon > i"));
+        wait.until(ExpectedConditions.elementToBeClickable(icon));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("window.scrollBy(0,2500)");
+    }
+
+    public void scrollPageDown(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement icon = driver.findElement(By.cssSelector("#menu-icon > i"));
+        wait.until(ExpectedConditions.elementToBeClickable(icon));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("var scrollBefore = $(window).scrollTop();" +
+                        "window.scrollTo(scrollBefore, document.body.scrollHeight);" +
+                        "return $(window).scrollTop() > scrollBefore;");
+    }
+
+    public void smallScroll(){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement icon = driver.findElement(By.cssSelector("#menu-icon > i"));
+        wait.until(ExpectedConditions.elementToBeClickable(icon));
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("window.scrollBy(0,500)");
+    }
+
+    public void scrollUntilVisible(WebElement element){
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+}
